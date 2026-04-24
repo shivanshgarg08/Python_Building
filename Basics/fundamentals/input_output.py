a0096a34053f5b98c27c40d92354bb9f7b47461e
shivanshgarg08 <shivansh.g2005@gmail.com>
2026-04-24 16:54:29 +0530

    program -2 done

--- Basics/fundamentals/input_output.py
+++ Basics/fundamentals/input_output.py
@@ -18,3 +18,37 @@ print(f"In 2027 you will be {age +1 } years old")
 """
 use of f strings
 """
+#Program-2 
+#Building a calculator 
+# taking user input using input()fn to get two seperate digits on which calculations to be performed
+# then using that digits performing arithmetic operations like +,-,/,* to get the result 
+# brute force attempt
+
+a = int(input("Enter the first number: "))
+b = int(input("Enter the second number: "))
+c = input("Enter +,-,/,* to perform arithmetic operation: ")
+
+if c=='/':
+    div = a/b
+    print(f"division = {div}")
+    if a/0:
+        print("Not defined")
+elif c=='*':
+    mul = a*b
+    print(f"Multiplication = {mul}")
+elif c=='+':
+    add = a+b
+    print(f"Addition = {add}")
+elif c=='-':
+    sub = a-b
+    print(f"Subtraction = {sub}")            
+#Program is working 
+
+#Perfect attempt 
+num1 = float(input("Enter the first number: "))
+num2 = float(input("Enter the second number"))
+print(f"Sub = {num1 - num2}")
+print(f"Add = {num1 + num2}")
+print(f"Mul = {num1*num2}")
+print(f"div = {num1/num2}")
+print(f"Not defined... {num1/0}") #solved the number/0 problem 
